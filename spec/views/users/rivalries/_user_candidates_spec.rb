d9a557d8882fd92bf15bc8d29e12cb9650fd5e32
glacials <qhiiyr@gmail.com>
2017-08-04 19:40:04 -0700

    Fix up users/rivalries/_{vs,user_candidates} tests

--- spec/views/users/rivalries/_user_candidates_spec.rb
+++ spec/views/users/rivalries/_user_candidates_spec.rb
@@ -2,8 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'users/rivalries/_user_candidates' do
   it 'renders user candidates' do
-    allow_any_instance_of(User).to receive(:follows).and_return(User)
-    render(partial: 'users/rivalries/user_candidates', locals: {user: FactoryGirl.create(:user), category: FactoryGirl.create(:category)})
+    user = FactoryGirl.create(:user)
+    allow(user).to receive(:follows).and_return(User.none)
+    render(partial: 'users/rivalries/user_candidates', locals: {
+      user: user,
+      category: FactoryGirl.create(:category)
+    })
 
     expect(view).to render_template('users/rivalries/_user_candidates')
   end
